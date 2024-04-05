10e8d2c03f7ac20091427d1d51e7b220a9dd4db0
Larissa Teixeira <lteixeira@4bio.com.br>
2024-04-05 16:02:39 -0300

    Aula 3: integração com api e funcionalidade de busca

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -4,8 +4,8 @@ import { Table } from './table/table'
 import { TableHeader } from './table/table-header'
 import { TableCell } from './table/table-cell'
 import { TableRow } from './table/table-row'
-import { ChangeEvent, useState } from 'react'
-import { attendees } from '../data/attendees'
+import { ChangeEvent, useEffect, useState } from 'react'
+// import { attendees } from '../data/attendees'   //Usado para importar lista de Participantes através do Faker
 import dayjs  from 'dayjs'
 import 'dayjs/locale/pt-br'
 import relativeTime from 'dayjs/plugin/relativeTime'
@@ -13,31 +13,92 @@ import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 dayjs.locale('pt-br')
 
+interface Attendee{
+    id: string
+    name: string
+    email: string
+    createdAt: string
+    checkedInAt: string | null
+}
+
 export function AttendeeList(){
-    const [search, setSearch] = useState('')
-    const [page, setPage] = useState(1)
+    const [search, setSearch] = useState(() => {
+        const url = new URL(window.location.toString())
 
-    const totalPages = Math.ceil(attendees.length / 10)
+        if(url.searchParams.has('search')){
+            return url.searchParams.get('search') ?? ''
+        }
 
+        return ''
+    })
+    const [page, setPage] = useState(() => {
+        const url = new URL(window.location.toString())
+
+        if(url.searchParams.has('page')){
+            return Number(url.searchParams.get('page'))
+        }
+
+        return 1
+    })
+    // const [attendees, setAttendees] = useState([]) //Usado para a versão com Faker
+    
+    const [total, setTotal] = useState(0)
+    const [attendees, setAttendees] = useState<Attendee[]>([])
+
+    // const totalPages = Math.ceil(attendees.length / 10) //Usado para a versão com Faker
+    const totalPages = Math.ceil(total / 10)
+
+    //conecta com a API
+    useEffect(() => {
+        const  url = new URL('http://localhost:3333/events/9e9bd979-9d10-4915-b339-3786b1634f33/attendees')
+        url.searchParams.set('pageIndex', String(page - 1))
+
+        if(search.length > 1){
+            url.searchParams.set('query', search)
+        }
+
+        fetch(url)
+            .then(response => response.json())
+            .then(data => { 
+                setAttendees(data.attendees) 
+                setTotal(data.total)
+            })
+    }, [page, search])
+
+    function setCurrentSearch(search: string){
+        const url = new URL(window.location.toString())
+        url.searchParams.set('search', search)
+        window.history.pushState({}, "", url) //Altera a URL sem recarregar a página
+        setSearch(search)
+    }
+
+    function setCurrentPage(page: number){
+        const url = new URL(window.location.toString())
+        url.searchParams.set('page', String(page))
+        window.history.pushState({}, "", url) 
+        setPage(page)
+    }
 
     function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>){
-        setSearch(event.target.value)
+        setCurrentSearch(event.target.value)
+        setCurrentPage(1)
     }
 
     function goToFirstPage(){
-        setPage(1)
+        // setPage(1) //Paginação sem parâmetros na URL
+        setCurrentPage(1)
     }
 
     function goToPreviousPage(){
-        setPage(page - 1)
+        setCurrentPage(page - 1)
     }
     
     function goToNextPage(){
-        setPage(page + 1)
+        setCurrentPage(page + 1)
     }
 
     function goToLastPage(){
-        setPage(totalPages)
+        setCurrentPage(totalPages)
     }
     
 
@@ -47,10 +108,8 @@ export function AttendeeList(){
                 <h1 className="text-2xl font-bold ">Participantes</h1>
                 <div className="px-3 w-72 py-1.5 border border-white/10 rounded-lg flex items-center gap-3">
                     <Search className='size-4 text-emerald-300' />
-                    <input onChange={onSearchInputChanged} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm" placeholder="Buscar participante..."></input>
+                    <input onChange={onSearchInputChanged} value={search} className="bg-transparent flex-1 outline-none border-0 p-0 text-sm focus:ring-0" placeholder="Buscar participante..."></input>
                 </div>
-
-                {search}
             </div>
 
                 <Table>
@@ -67,7 +126,8 @@ export function AttendeeList(){
                         </tr>
                     </thead>
                     <tbody>
-                        { attendees.slice((page - 1) * 10, page * 10).map((attendee)=>{
+                        {/* { attendees.slice((page - 1) * 10, page * 10).map((attendee)=>{ //Paginação via front-end */}
+                        {attendees.map((attendee) => {
                             return (
                                 <TableRow key={attendee.id}>
                                     <TableCell>
@@ -81,7 +141,11 @@ export function AttendeeList(){
                                         </div>
                                     </TableCell>
                                     <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
-                                    <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
+                                    <TableCell>
+                                        {attendee.checkedInAt === null 
+                                        ? <span className='text-zinc-400'>Check-in Pendente</span>
+                                        : dayjs().to(attendee.checkedInAt)}
+                                        </TableCell>
                                     <TableCell>
                                         <IconButton transparent={true}>
                                             <MoreHorizontal className='size-4' />
@@ -94,7 +158,8 @@ export function AttendeeList(){
                     <tfoot>
                         <tr>
                             <TableCell colSpan={3}>
-                                Mostrando 10 de {attendees.length} itens
+                                {/* Mostrando 10 de {attendees.length} itens //Usado para a versão com Faker */}
+                                Mostrando {attendees.length} de {total} itens
                             </TableCell>
                             <TableCell colSpan={3} className='text-right'>
                                 <div className='inline-flex items-center gap-8'>
